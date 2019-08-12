e7ee72e41b898ef7b19a175e0d88ef453eec4931
Darkxiete <470058433@qq.com>
2019-08-13 00:26:27 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+struct free_throws {
+    std::string name;
+    int made;
+    int attempts;
+    float percent;
+};
+
+free_throws &accumulate_1(free_throws &target, const free_throws &source);
+
+free_throws accumulate_2(free_throws &target, const free_throws &source);
+
+
+free_throws &accumulate_1(free_throws &target, const free_throws &source) {
+    target.attempts += source.attempts;
+    target.made += source.made;
+    return target;
+}
+
+free_throws accumulate_2(free_throws &target, const free_throws &source) {
+    target.attempts += source.attempts;
+    target.made += source.made;
+    return target;
+}
+
+int main() {
+    free_throws one = {"Ifelas Branch", 13, 14};
+    free_throws two = {"Andor Knott", 10, 16};
+    free_throws three = {"Minnie Max", 7, 9};
+    free_throws a = accumulate_1(one, two);
+    free_throws b = accumulate_2(two, three);
+}
