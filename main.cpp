a468ec3eb6e17c1964db04bc00587e53a9e3be56
Darkxiete <470058433@qq.com>
2019-08-14 20:32:48 +0800

    Update

--- main.cpp
+++ main.cpp
@@ -11,6 +11,10 @@ free_throws &accumulate_1(free_throws &target, const free_throws &source);
 
 free_throws accumulate_2(free_throws &target, const free_throws &source);
 
+void swap_1(int *pa, int *pb);
+
+void swap_2(int &pa, int &pb);
+
 
 free_throws &accumulate_1(free_throws &target, const free_throws &source) {
     target.attempts += source.attempts;
@@ -24,10 +28,37 @@ free_throws accumulate_2(free_throws &target, const free_throws &source) {
     return target;
 }
 
+void swap_1(int *pa, int *pb) {
+    int a = *pa;
+    int b = *pb;
+    *pa = b;
+    *pb = a;
+}
+
+void swap_2(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void swap_3(int *pa, int *pb) {
+    int temp = *pa;
+    *pa = *pb;
+    *pb = temp;
+}
+
 int main() {
     free_throws one = {"Ifelas Branch", 13, 14};
     free_throws two = {"Andor Knott", 10, 16};
     free_throws three = {"Minnie Max", 7, 9};
-    free_throws a = accumulate_1(one, two);
-    free_throws b = accumulate_2(two, three);
+    int a = 1, b = 2;
+
+    free_throws fa = accumulate_1(one, two);
+    free_throws fb = accumulate_2(two, three);
+
+
+    std::cout << "a, b = " << a << ", " << b << std::endl;
+    std::cout << "a, b = " << a << ", " << b << std::endl;
+
+
 }
